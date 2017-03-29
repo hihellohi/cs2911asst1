1d5d606d07e5aa5a717d1810eba3ece935fdd5e1
Kevin <hihellohi610@gmail.com>
2017-03-30 09:51:29 +1100

    added uml and depot

---
+++ src/Depot.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ * Represents a depot in the system
+ *
+ * @author	Kevin Ni
+ * @since	2017-03-30
+ */
+public class Depot{
+	public Depot(String depotName){
+		name = depotName;
+	}
+
+	public AddVan(String Vanname, boolean auto){
+	}
+
+	String name;
+}
